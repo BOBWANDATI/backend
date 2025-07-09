d309739dfcfc48e155c22c345af43b7b95462cd9
BOB WANDATI <153294508+BOBWANDATI@users.noreply.github.com>
2025-07-09 21:20:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,8 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('✅ MongoDB connected');
   const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => {
-    console.log(🚀 Server running on http://localhost:${PORT});
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+
   });
 })
 .catch((err) => {
